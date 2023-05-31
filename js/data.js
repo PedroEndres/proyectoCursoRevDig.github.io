5982a18cea714a324567e6af5bc4f7a62fc553f3
Pedro Endres <86131035+PedroEndres@users.noreply.github.com>
2023-05-31 15:11:30 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,91 @@
+const menu = [
+  {
+    img: "../img/sandwichMushrooms.jpg",
+    alt: "Sandwich mushrooms",
+    type: "sandwich",
+    title: "CLUBHOUSE XXL",
+    description:
+      "😋 ¿Qué trae? 👉🏻 Sándwich en pan lactal con queso, huevo, tomate, rúcula, cebolla y guacamole. 🍟¡Obvio sale con fritas!",
+  },
+  {
+    img: "./img/sandwichDeMiga.jpg",
+    alt: "Sandwich de miga",
+    type: "sandwich",
+    title: "Sándwich de miga",
+    description: `🙌Habemus sándwiches de miga😋 👉Rúcula, tomate y queso.👉Remolacha, zanahoria y hummus de garbanzos. 👉Apio, roquefort,
+    nueces y manzana verde. 👉Zanahoria y berenjena. 🤪 Qué opción
+    elegís? 💥Están explotadísimos⚡`,
+  },
+  {
+    img: "./img/sandwichSeitán.jpg",
+    alt: `Sandwich de seitán`,
+    type: "sandwich",
+    title: "Sándwich de seitán",
+    description: ` 😋 Salen unos sándwich de seitán con amigxs? 🥪 Mila de seitán,
+    cheddar a base de almendras, rúcula, tomate y cebolla caramelizada.
+    🤞Date el gusto de comer algo rico y PEDILO!`,
+  },
+  {
+    img: "./img/sandwichConPapas.jpg",
+    alt: "sandwich con papas",
+    type: "sandwich",
+    title: "Sándwich con Papas",
+    description: ` 😋 Salen unos sándwich de seitán con amigxs? 🥪 Mila de seitán,
+    cheddar a base de almendras, rúcula, tomate y cebolla caramelizada.
+    🤞Date el gusto de comer algo rico y PEDILO!`,
+  },
+  {
+    img: "./img/burguerVegana.jpg",
+    alt: "Hamburguesa Vegana",
+    type: "hamburguesa",
+    title: "Burguer 100% vegana",
+    description: `🔥¡Viernes ATR y con nueva burguer 100% vegana!🍔
+    🤘Doble medallón de arroz, remolacha y zanahoria.
+    🤘Queso cremoso vegano.
+    🤘Mayonesa de zanahoria.
+    🤘Cebolla crispy.
+    🤘Rúcula.
+    🤘Tomate.
+    🍟SALE CON ALTAS PAPAS🍟`,
+  },
+  {
+    img: "./img/wokVegetales.jpg",
+    alt: "Wok de vegetales",
+    type: "wok",
+    title: "Wok de vegetales",
+    description: `✌️ Arrancamos la semana con uno de sus platos favoritos ➡️ nuestro wok de vegetales.
+    🌱 ¿Sabías que es 100% vegano? 😏
+    📩 Hacenos tu pedido por Whatsapp`,
+  },
+  {
+    img: "./img/risotto.jpg",
+    alt: "Risotto",
+    type: "risotto",
+    title: "Risotto",
+    description: `🍽️¿Ya sabes qué cenar hoy?
+    👉Nosotros te proponemos un buen plato de risotto con champiñones😋
+    🚨¡Llevate 2 porciones a sólo $700🚨`,
+  },
+  {
+    img: "./img/pizza.jpg",
+    alt: "Pizza",
+    type: "pizza",
+    title: "Pizza",
+    description: `⚡Sábado lluvioso pero en Punto Verde le metemos onda igual🤟
+
+    👉 Si no sabes qué comer hoy andá a chequear nuestras historias que hay unas promos que te van a encantar💣`,
+  },
+  {
+    img: "./img/wrapTurbo.jpg",
+    alt: "Wrap",
+    type: "wrap",
+    title: "Wrap Turbo",
+    description: `🚀Por fin sábado⚡
+    💥Planazo: pedir en Punto Verde 🍃 y tomar algo rico en el balcón 😋
+    👉 Éste es nuestro wrap turbo que es una 💣.
+    ▪️Masa vegana con porotos, carne de soja, vegetales, fideos de arroz y guacamole.
+    🌯El favorito de muchos🌯`,
+  },
+];
+
+export default menu;
---
+++ js/index.js
@@ -0,0 +1,77 @@
+import data from "./data.js";
+
+function bienvenida() {
+  let edad = prompt("ingresar edad: ");
+  if (edad >= 18) {
+    alert("bienvenid@!");
+  } else {
+    alert("Usted ingresa bajo su reponsabilidad");
+  }
+}
+
+// bienvenida();
+
+function localidad() {
+  let distancia = confirm("¿Vive a menos de 40km de San Nicolas?");
+  if (distancia == true) {
+    alert("Su envio será gratis!");
+  } else {
+    alert("Su envio tendrá un costo adicional");
+  }
+}
+
+// localidad();
+
+let sectionMenu = document.getElementById("menu");
+let btnTodos = document.getElementById("todos"); //cambiar Nombres
+let btnSandwich = document.getElementById("sandwich");
+let btnHamburguesa = document.getElementById("hamburguesa");
+let btnWok = document.getElementById("wok");
+let btnRisotto = document.getElementById("risotto");
+let btnPizza = document.getElementById("pizza");
+let btnWrap = document.getElementById("wrap");
+
+btnTodos.addEventListener("click", () => Validar("todos"));
+btnSandwich.addEventListener("click", () => Validar("sandwich"));
+btnHamburguesa.addEventListener("click", () => Validar("hamburguesa"));
+btnWok.addEventListener("click", () => Validar("wok"));
+btnRisotto.addEventListener("click", () => Validar("risotto"));
+btnPizza.addEventListener("click", () => Validar("pizza"));
+btnWrap.addEventListener("click", () => Validar("wrap"));
+
+function Validar(filtro) {
+  let menu = data;
+  sectionMenu.innerHTML = "";
+  console.log(filtro);
+  if (filtro == "sandwich") {
+    menu = data.filter((element) => element.type == "sandwich");
+  } else if (filtro == "hamburguesa") {
+    menu = data.filter((element) => element.type == "hamburguesa");
+  } else if (filtro == "wok") {
+    menu = data.filter((element) => element.type == "wok");
+  } else if (filtro == "risotto") {
+    menu = data.filter((element) => element.type == "risotto");
+  } else if (filtro == "pizza") {
+    menu = data.filter((element) => element.type == "pizza");
+  } else if (filtro == "wrap") {
+    menu = data.filter((element) => element.type == "wrap");
+  }
+  menu.map((element) => {
+    sectionMenu.innerHTML += `<article class="article-menu">
+      <img
+        src=${element.img}
+        alt=${element.alt}
+        class="img-menu"
+      />
+      <span class="tag">${element.type}</span>
+      <div class="info-menu">
+        <h2>${element.title}</h2>
+        <p>
+         ${element.description}
+        </p>
+      </div>
+      </article>`;
+  });
+}
+
+Validar("todos");
